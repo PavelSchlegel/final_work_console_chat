5c345e100ff80b247eeab5fe77e5ceb5ef335288
pinegin pavel <pavel.pinegin.essen@gmail.com>
2023-09-18 22:49:31 +0200

    net_client

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include <chat/iserver.hpp>
-#include <boost/array.hpp>
+#include <chat/net_client.hpp>
+#include <boost/asio/spawn.hpp>
+#include <iostream>
+#include <chat/server.hpp>
 
-int main ()
+int main (int argc, char* argv[])
 {
+    using boost::asio::ip::tcp;
+    chat::Server server;
+    try {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: chat_server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        boost::asio::spawn(
+            io_context,
+            [&](boost::asio::yield_context yield)
+            {
+                tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
+
+                for (;;) {
+                    boost::system::error_code ec;
+                    tcp::socket socket(io_context);
+                    acceptor.async_accept(socket, yield[ec]);
+                    if (!ec)
+                    {
+                        std::make_shared<chat::NetClient>(server, std::move(socket))->go(io_context);
+                    }
+                }
+            },
+            [](std::exception_ptr e)
+            {
+                if (e) {
+                    std::rethrow_exception(e);
+                }
+            }
+        );
+
+        io_context.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
     return 0;
 }
---
+++ src/chat/net_client.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include <memory>
+#include "iserver.hpp"
+#include "iclient.hpp"
+#include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+
+namespace chat {
+    using boost::asio::ip::tcp;
+
+    class NetClient: public chat::IClient, public std::enable_shared_from_this<NetClient>
+    {
+    private:
+        tcp::socket m_socket;
+        IServerHandle& m_server_handle;
+    public:
+        NetClient(chat::IServer& server, tcp::socket socket)
+        : m_socket(std::move(socket))
+        , m_server_handle(server.connect(*this))
+        {
+            
+        }
+
+        ~NetClient()
+        {
+            m_server_handle.disconnect();
+        }
+
+        void msg_recv(const std::string& who, const std::string& msg) override
+        {
+            //wriete
+        }
+
+        void go(boost::asio::io_context& io_context)
+        {
+            auto self(shared_from_this());
+            boost::asio::spawn(
+                io_context, 
+                [this, self](boost::asio::yield_context yield)
+                {
+                    try
+                    {
+                        char data[128];
+                        for (;;) {
+                            std::size_t n = m_socket.async_read_some(boost::asio::buffer(data), yield);
+
+                            //вызовы server_handle
+                        }
+                    }
+                    catch (std::exception& e)
+                    {
+                        m_socket.close();
+                    }
+                },
+                boost::asio::detached
+            );
+        }
+    };
+} // namespace chat
